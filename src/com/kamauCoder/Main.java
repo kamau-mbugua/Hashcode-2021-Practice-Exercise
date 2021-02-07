aa1abad1136c24cfa28686fa91337c7fa926368f
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-08 00:01:05 +0300

    Created Class DeliveredPizza

--- src/com/kamauCoder/Main.java
+++ src/com/kamauCoder/Main.java
@@ -1,5 +1,10 @@
 package com.kamauCoder;
 
+import java.awt.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,7 +12,15 @@ public class Main {
         System.out.println("Hello Google HashCode 2021");
         Service service = new Service();
         try{
+            Path inputPath = Paths.get("src","in");
+            Path outputPath = Paths.get("src","out");
 
+            Files.newDirectoryStream(inputPath,
+                    path -> path.toString().endsWith(".txt"))
+                    .forEach(path -> {
+                        service.readInputFile(path);
+                        List<DeliveredPizza> pizzas = service.process();
+                    });
 
         }
     }
--- src/com/kamauCoder/Service.java
+++ src/com/kamauCoder/Service.java
@@ -1,4 +1,8 @@
 package com.kamauCoder;
 
+import java.nio.file.Path;
+
 public class Service {
+    public void readInputFile(Path path) {
+    }
 }
