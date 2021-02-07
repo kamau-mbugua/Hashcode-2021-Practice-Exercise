35a55e47efb69b6faee1cfafa1d87cae1699f930
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-07 23:48:30 +0300

    Added b_little_bit_of_everything dataset file

--- src/com/kamauCoder/Main.java
+++ src/com/kamauCoder/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Google HashCode 2021");
-        Service
+        Service service = new Service();
+        try{
+
+        }
     }
 }
