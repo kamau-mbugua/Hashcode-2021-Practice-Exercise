b13542dad83e936c4b3220d7c66a1b5edd3988c2
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-08 00:09:05 +0300

    Created class Pizza

---
+++ src/com/kamauCoder/Pizza.java
@@ -0,0 +1,4 @@
+package com.kamauCoder;
+
+public class Pizza {
+}
--- src/com/kamauCoder/Service.java
+++ src/com/kamauCoder/Service.java
@@ -4,6 +4,13 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class Service {
+    int pizzaType;
+    int teamOf2;
+    int teamOf3;
+    int teamOf4;
+    Pizza[] pizzas;
+
+
     public void readInputFile(Path path) {
     }
 
