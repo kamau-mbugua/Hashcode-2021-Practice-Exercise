6b79e94c69b6558b1acd228119a01b7afd5ca528
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-02-08 00:16:17 +0300

    Created and implemented an override String ; toString() class DeliveredPizza

--- src/com/kamauCoder/DeliveredPizza.java
+++ src/com/kamauCoder/DeliveredPizza.java
@@ -1,4 +1,16 @@
 package com.kamauCoder;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
 public class DeliveredPizza {
+    public int team;
+    public List<Integer> pizzaIndexes;
+
+    @Override
+    public String toString() {
+        return team+" "+pizzaIndexes.stream().map(x ->x.toString()).collect(joining(" "));
+
+    }
 }
--- src/com/kamauCoder/Service.java
+++ src/com/kamauCoder/Service.java
@@ -1,6 +1,9 @@
 package com.kamauCoder;
 
+import java.lang.reflect.Array;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Service {
@@ -15,6 +18,12 @@ public class Service {
     }
 
     public List<DeliveredPizza> process() {
+        List<DeliveredPizza> deliveredPizzasResults = new ArrayList<>();
+
+        DeliveredPizza deliveredPizza = new DeliveredPizza();
+        deliveredPizza.team =2;
+        deliveredPizza.pizzaIndexes = Arrays.asList(1, 2, 3);
+        deliveredPizzasResults.add(deliveredPizza);
     }
 
     public void writeOutputFile(Path resolve, List<DeliveredPizza> pizzas) {
